67d09eb6b4522018b9da0dcfc0a496f76afa2f74
Martha659 <martha.blackcloud@students.msswarriors.org>
2026-02-18 17:44:07 +0000

    11

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Put your code here
+let total = 0;
+
+function addToCart(name, price) {
+const cartList = document.getElementById('cartList');
+const totalDisplay = document.getElementById('total');
+
+const li = document.createElement('li');
+li.textContent = `${name} - $${price}`;
+cartList.appendChild(li);
+
+total += price;
+totalDisplay.textContent = total;
+}
